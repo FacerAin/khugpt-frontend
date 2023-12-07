fe4e491dcfb43c056d3369d773d7838a292a7e5d
FacerAin <ywsong.dev@kakao.com>
2023-12-07 13:11:59 +0900

    feat: update user interface

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
-import 'dart:convert';
 import 'dart:io';
 import 'session.dart';
+import 'dart:async';
+import "dart:math";
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import 'package:http/http.dart' as http;
@@ -40,6 +40,30 @@ class _ChatPageState extends State<ChatPage> {
   );
 
   final _agent = const types.User(id: '1234');
+
+  int _seconds = 0;
+  bool _isRunning = false;
+  late Timer _timer;
+
+  void _startTimer() {
+    _isRunning = true;
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        _seconds++;
+      });
+    });
+  }
+
+  void _stopTimer() {
+    _isRunning = false;
+    _timer.cancel();
+  }
+
+  void _resetTimer() {
+    setState(() {
+      _seconds = 0;
+    });
+  }
 
   @override
   void initState() {
@@ -134,12 +158,13 @@ class _ChatPageState extends State<ChatPage> {
   ) {
     final index = _messages.indexWhere((element) => element.id == message.id);
     final updatedMessage = (_messages[index] as types.TextMessage).copyWith(
-      previewData: previewData,
+      previewData: previewData.copyWith(title: ""),
     );
-
-    setState(() {
-      _messages[index] = updatedMessage;
-    });
+    if (previewData.title == "404 Not Found") {
+      setState(() {
+        _messages[index] = updatedMessage;
+      });
+    }
   }
 
   types.TextMessage _buildUserMessage(String text) {
@@ -164,19 +189,35 @@ class _ChatPageState extends State<ChatPage> {
 
   String _transformReponseText(String text) {
     text = text.replaceAll(")", " )");
+    text = text.replaceAll("(", "( ");
     return text;
   }
 
   void _handleSendPressed(types.PartialText message) async {
     Map<String, String> requestData = {'query': message.text};
     _addMessage(_buildUserMessage(message.text));
+    _addMessage(_buildAgentMessage("✅ 답변 생성 중... 잠시만 기다려 주세요 ☺️"));
+
+    _startTimer();
     final dynamic response = await Session().post(
         'http://facerain-dev.iptime.org:1009/api/v1/chat/completion',
         requestData);
 
+    _stopTimer();
+
+    if (_seconds < 5) {
+      await Future.delayed(const Duration(seconds: 2));
+    }
+
     String agentMessageText = _transformReponseText(response['answer']);
 
+    if (_seconds > 5) {
+      _addMessage(_buildAgentMessage("✅ 답변 생성 완료"));
+    }
+
     _addMessage(_buildAgentMessage(agentMessageText));
+
+    _resetTimer();
   }
 
   void _initMessages() async {
@@ -186,9 +227,27 @@ class _ChatPageState extends State<ChatPage> {
   }
 
   void _loadMessages() async {
+    List<String> exampleQueryList = [
+      "컴퓨터공학과 졸업 요건에 대해 알려줘",
+      "올해 SW 페스티벌 언제 열렸어?",
+      "개발 관련된 취업이나 인턴 정보가 있을까?",
+      "인공지능학과 전공 필수 과목 뭐 있어?",
+      "SW 관련 대회 3개 추천해줘",
+      "SW 관련된 봉사 활동 없을까",
+      "올해 가을프로그래밍 경시대회 신청 링크 알려줘",
+      "내년 대학원 모집 언제부터야?"
+    ];
+
+    final random = Random();
+    String exampleQuery1 =
+        exampleQueryList[random.nextInt(exampleQueryList.length)];
+
     _addMessage(_buildAgentMessage("안녕하세요, KHUGPT입니다! 무엇이 궁금한가요?"));
+    _addMessage(_buildAgentMessage("이런 질문을 해볼 수 있어요 ✅"));
+    _addMessage(_buildAgentMessage("Q. $exampleQuery1"));
   }
 
+//give me some link about programming contest
   Widget _bubbleBuilder(
     Widget child, {
     required message,
@@ -213,13 +272,21 @@ class _ChatPageState extends State<ChatPage> {
         appBar: AppBar(
           centerTitle: true,
           backgroundColor: const Color.fromRGBO(160, 16, 26, 1),
-          title: const Text('KHUGPT'),
+          title: const Text(
+            'KHUGPT',
+            style: TextStyle(color: Colors.white),
+          ),
         ),
         body: Chat(
           theme: const DefaultChatTheme(
               receivedMessageBodyTextStyle: TextStyle(color: Colors.white),
               primaryColor: Colors.white,
               secondaryColor: Colors.white,
+              receivedMessageBodyLinkTextStyle: TextStyle(
+                color: Colors.white,
+                decoration: TextDecoration.underline,
+                decorationColor: Colors.white,
+              ),
               attachmentButtonIcon: Icon(
                 Icons.add,
                 color: Colors.white,
